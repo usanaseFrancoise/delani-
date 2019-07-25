76f984eb76eb3fca33fd1c5e6fc36adf52fe81b5
usanase Francoise <fusanasencoise@gmail.com>
2019-07-25 16:58:03 +0200

    edits

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(".click1").click(function() {
+        $("#show1").toggle ();
+        $("#toggle1").toggle ();  
+          
+    });
+
+$(".click2").click(function() {
+    $("#show2").toggle ();
+    $("#toggle2").toggle ();        
+});
+$(".click3").click(function() {
+    $("#show3").toggle ();
+    $("#toggle3").toggle ();        
+});
+});
