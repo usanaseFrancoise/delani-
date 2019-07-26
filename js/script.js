4fe8e982519c45fed68aca8c73bb2816f3d572e6
usanase Francoise <fusanasencoise@gmail.com>
2019-07-26 15:33:27 +0200

    add

--- js/script.js
+++ js/script.js
@@ -17,4 +17,108 @@ $(".click3").click(function() {
 $(".once").click(function(){
     alert("thank you for sending the message");
 });
+$('.image1').hide();
+$('#photos1').animate({
+    opacity:1
+});
+$('#photos1').hover(function(){
+    $(this).stop().animate({opacity:0.3},200);
+        $('.image1').fadeIn();
+
+}, function(){
+$(this).stop().animate({
+        opacity: 3},200);
+        $('.image1').fadeOut();
+ });
+ $('.image2').hide();
+$('#photos2').animate({
+    opacity:1
+});
+$('#photos2').hover(function(){
+    $(this).stop().animate({opacity:0.3},200);
+        $('.image2').fadeIn();
+
+}, function(){
+$(this).stop().animate({
+        opacity: 3},200);
+        $('.image2').fadeOut();
+ });
+ $('.image3').hide();
+$('#photos3').animate({
+    opacity:1
+});
+$('#photos3').hover(function(){
+    $(this).stop().animate({opacity:0.3},200);
+        $('.image3').fadeIn();
+
+}, function(){
+$(this).stop().animate({
+        opacity: 3},200);
+        $('.image3').fadeOut();
+ });
+ $('.image4').hide();
+$('#photos4').animate({
+    opacity:1
+});
+$('#photos4').hover(function(){
+    $(this).stop().animate({opacity:0.3},200);
+        $('.image4').fadeIn();
+
+}, function(){
+$(this).stop().animate({
+        opacity: 3},200);
+        $('.image4').fadeOut();
+ });
+ $('.image5').hide();
+$('#photo1').animate({
+    opacity:1
+});
+$('#photo1').hover(function(){
+    $(this).stop().animate({opacity:0.3},200);
+        $('.image5').fadeIn();
+
+}, function(){
+$(this).stop().animate({
+        opacity: 3},200);
+        $('.image5').fadeOut();
+ });
+ $('.image6').hide();
+$('#photo2').animate({
+    opacity:1
+});
+$('#photo2').hover(function(){
+    $(this).stop().animate({opacity:0.3},200);
+        $('.image6').fadeIn();
+
+}, function(){
+$(this).stop().animate({
+        opacity: 3},200);
+        $('.image6').fadeOut();
+ });
+ $('.image7').hide();
+$('#photo3').animate({
+    opacity:1
+});
+$('#photo3').hover(function(){
+    $(this).stop().animate({opacity:0.3},200);
+        $('.image7').fadeIn();
+
+}, function(){
+$(this).stop().animate({
+        opacity: 3},200);
+        $('.image7').fadeOut();
+ });
+ $('.image8').hide();
+$('#photo4').animate({
+    opacity:1
+});
+$('#photo4').hover(function(){
+    $(this).stop().animate({opacity:0.3},200);
+        $('.image8').fadeIn();
+
+}, function(){
+$(this).stop().animate({
+        opacity: 3},200);
+        $('.image8').fadeOut();
+ });
 });
